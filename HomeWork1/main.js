a98535e3e5d44bdf2b1cad67b835cd4f17abf9be
SashaKukurika <Kukurika.sasha@gmail.com>
2022-11-01 15:59:27 +0200

    Done HW 1

---
+++ HomeWork1/main.js
@@ -0,0 +1,49 @@
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+const PI = 3.14;
+let num5 = 2.7;
+let num6 = 16;
+let bool1 = true;
+let bool2 = false;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(PI);
+console.log(bool1);
+console.log(bool2);
+
+
+let firstName = 'Oleksandr';
+let middleName = 'Kukurika';
+let lastName = 'Hryhorovych';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let name = prompt('Як тебе звати?');
+let secondname = prompt('Яка у тебе фамілія?');
+let age = prompt('Скільки тобі років?');
+
+alert(`Тебе звати ${name}, твоя фамілія ${secondname} і тобі ${age} років!`);
