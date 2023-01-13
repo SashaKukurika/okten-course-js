166082c42741df9e947aa5b967911cb7999e0df9
SashaKukurika <Kukurika.sasha@gmail.com>
2023-01-13 19:49:12 +0200

    done 2 and 3 task from ADD 12 1 at progress

--- Additional12/main.js
+++ Additional12/main.js
@@ -12,16 +12,16 @@ fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json()).
 
         let btn = document.createElement('button');
         btn.innerText = 'Posts';
-        let divPost = document.createElement('div');
         div.appendChild(btn);
+        let divPost = document.createElement('div');
 
-        let numOfClick = 0;
+        let numOfClick1 = 0;
         btn.onclick = function () {
 
-            ++numOfClick;
-            if (numOfClick === 1) {
-                let id = user.id;
-                fetch('https://jsonplaceholder.typicode.com/users/' + id + '/posts').then(value => value.json())
+            ++numOfClick1;
+            if (numOfClick1 === 1) {
+
+                fetch('https://jsonplaceholder.typicode.com/users/' + user.id + '/posts').then(value => value.json())
                     .then(posts => {
                         for (const post of posts) {
 
@@ -34,30 +34,28 @@ fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json()).
                             let btn2 = document.createElement('button');
                             divPost.appendChild(btn2);
                             btn2.innerText = `Comments ${post.id}`;
-                            btn2.id = post.id;
                             let divComments = document.createElement('div');
-                            divPost.appendChild(divComments);
 
                             // як отримувати з батонна по якому клікнули його айді щоб передати далі в фетч??
 
-                            let numOfClick = 0;
-                            btn2.onclick = function (e) {
+                            let numOfClick2 = 0;
+                            btn2.onclick = function () {
 
-                                let postId = e.target.id
-                                ++numOfClick;
+                                ++numOfClick2;
+                                if (numOfClick2 === 1) {
 
-                                if (numOfClick === 1) {
-                                    fetch('https://jsonplaceholder.typicode.com/posts/' + postId + '/comments').then(value => value.json())
+                                    fetch('https://jsonplaceholder.typicode.com/comments?postId=' + post.id).then(value => value.json())
                                         .then(comments => {
-                                            for (const comment of comments) {
 
+                                            for (const comment of comments) {
                                                 divPost.appendChild(divComments);
+
                                                 for (const commentKey in comment) {
                                                     divComments.innerHTML += `<h3>${commentKey}: ${comment[commentKey]}</h3>`;
                                                 }
                                             }
                                         })
-                                } else if (numOfClick % 2 === 0) {
+                                } else if (numOfClick2 % 2 === 0) {
                                     divComments.style.display = 'none';
                                 } else {
                                     divComments.style.display = 'block';
@@ -67,7 +65,7 @@ fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json()).
 
                         }
                     })
-            } else if (numOfClick % 2 === 0) {
+            } else if (numOfClick1 % 2 === 0) {
                 divPost.style.display = 'none';
             } else {
                 divPost.style.display = 'block';
@@ -85,42 +83,42 @@ fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json()).
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
 
-fetch('https://jsonplaceholder.typicode.com/posts').then(value => value.json()).then(posts => {
-    for (const post of posts) {
-        let div = document.createElement('div');
-        document.body.appendChild(div);
-        div.innerText += `${post.id} - ${post.title}`;
-
-        let btn = document.createElement('button');
-        btn.innerText = 'Comments';
-        let div2 = document.createElement('div');
-        div.appendChild(btn);
-
-        let numOfClick = 0;
-        btn.onclick = function () {
-
-            ++numOfClick;
-            if (numOfClick === 1) {
-                let id = post.id;
-                fetch('https://jsonplaceholder.typicode.com/posts/' + id + '/comments').then(value => value.json())
-                    .then(comments => {
-                        for (const comment of comments) {
-
-                            div.appendChild(div2);
-                            for (const commentKey in comment) {
-                                div2.innerHTML += `<h3>${commentKey}: ${comment[commentKey]}</h3>`;
-                            }
-                        }
-                    })
-            } else if (numOfClick % 2 === 0) {
-                div2.style.display = 'none';
-            } else {
-                div2.style.display = 'block';
-            }
-        }
-    }
-
-})
+// fetch('https://jsonplaceholder.typicode.com/posts').then(value => value.json()).then(posts => {
+//     for (const post of posts) {
+//         let div = document.createElement('div');
+//         document.body.appendChild(div);
+//         div.innerText += `${post.id} - ${post.title}`;
+//
+//         let btn = document.createElement('button');
+//         btn.innerText = 'Comments';
+//         let div2 = document.createElement('div');
+//         div.appendChild(btn);
+//
+//         let numOfClick = 0;
+//         btn.onclick = function () {
+//
+//             ++numOfClick;
+//             if (numOfClick === 1) {
+//
+//                 fetch('https://jsonplaceholder.typicode.com/comments?postId=' + post.id).then(value => value.json())
+//                     .then(comments => {
+//                         for (const comment of comments) {
+//
+//                             div.appendChild(div2);
+//                             for (const commentKey in comment) {
+//                                 div2.innerHTML += `<h3>${commentKey}: ${comment[commentKey]}</h3>`;
+//                             }
+//                         }
+//                     })
+//             } else if (numOfClick % 2 === 0) {
+//                 div2.style.display = 'none';
+//             } else {
+//                 div2.style.display = 'block';
+//             }
+//         }
+//     }
+//
+// })
 
 // - Імітуємо наповнення інтернет магазину товарами :
 //   Створити форму з наступними полями :
@@ -132,16 +130,16 @@ fetch('https://jsonplaceholder.typicode.com/posts').then(value => value.json()).
 // створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на
 // сторінці всі товари.
 
-let arrOfProducts = [];
-localStorage.setItem('basket', JSON.stringify(arrOfProducts));
-
-document.querySelector('.btn').onclick = function () {
-    let product = {
-        name: document.querySelector('.name').value,
-        quantity: document.querySelector('.numberOfProduct').value,
-        price: document.querySelector('.price').value,
-        image: document.querySelector('.logo').value
-    }
-    arrOfProducts.push(product);
-    localStorage.setItem('basket', JSON.stringify(arrOfProducts));
-}
+// let arrOfProducts = [];
+// localStorage.setItem('basket', JSON.stringify(arrOfProducts));
+//
+// document.querySelector('.btn').onclick = function () {
+//     let product = {
+//         name: document.querySelector('.name').value,
+//         quantity: document.querySelector('.numberOfProduct').value,
+//         price: document.querySelector('.price').value,
+//         image: document.querySelector('.logo').value
+//     }
+//     arrOfProducts.push(product);
+//     localStorage.setItem('basket', JSON.stringify(arrOfProducts));
+// }
