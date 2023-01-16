15b5c2e778002c69c5baf9889f43b12d7ae0f475
SashaKukurika <Kukurika.sasha@gmail.com>
2023-01-16 22:38:51 +0200

    at progress add 10

--- Additional10/main.js
+++ Additional10/main.js
@@ -4,38 +4,170 @@
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
 
-
+// let arr = ['недоразвите', 'бовдур', 'нікчема', 'рахіт'];
+//
+// let input = document.createElement('input');
+// let btn = document.createElement("button");
+// btn.innerText = 'Check for bad word';
+// document.body.append(input, btn);
+//
+// btn.onclick = function () {
+//
+//     console.log(input.value);
+//     for (const string of arr) {
+//
+//         if (string === input.value) {
+//             alert('It is bad word');
+//         }
+//     }
+// }
 
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+
+// let br = document.createElement("br");
+// let inputSentence = document.createElement("input");
+// let buttonSentence = document.createElement("button");
+// buttonSentence.innerText = 'Check for bad word';
+// document.body.append(br, inputSentence, buttonSentence);
 //
+// buttonSentence.onclick = function () {
+//     let valueArr = inputSentence.value.split(' ');
 //
-//
-//
+//     for (const string of valueArr) {
+//         for (const badWord of arr) {
+//             if (string === badWord) {
+//                 alert('It is bad word in your sentence');
+//             }
+//         }
+//     }
+// }
+
+
 // - Создайте меню, которое раскрывается/сворачивается при клике
+
+// let br2 = document.createElement("br");
+// let menuDiv = document.createElement("div");
+// menuDiv.style.textAlign = 'center';
+// menuDiv.innerText = 'Menu';
+// menuDiv.style.background = 'silver';
+// menuDiv.style.height = '20px';
+// menuDiv.style.width = '150px';
+// document.body.append(br2, menuDiv);
+// let numOfClicks = 0;
+// let element = document.createElement("div");
 //
+// menuDiv.onclick = function () {
+//     numOfClicks++;
+//     console.log(numOfClicks);
 //
+//     if (numOfClicks === 1) {
+//
+//         menuDiv.appendChild(element);
+//         element.style.background = 'blue';
+//         element.style.height = 'auto';
+//         element.style.width = 'auto';
+//
+//         for (let i = 1; i < 5; i++) {
+//             let li = document.createElement('li');
+//             element.appendChild(li);
+//             li.innerText = `Section ${i}`;
+//         }
+//     } else if (numOfClicks % 2 === 0) {
+//         element.style.display = 'none';
+//     } else {
+//         element.style.display = 'block';
+//     }
+//
+// }
+
 // - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 //     Вывести список комментариев в документ, каждый в своем блоке.
 //     Добавьте каждому комментарию по кнопке для сворачивания его body.
-//
-//
+
+let commentsArr = [
+    {
+        title: "dignissimos et",
+        body: "exercitationem et id quae cum omnis\nvoluptatibus accusantium et quidem\nut ipsam sint\ndoloremque illo ex atque necessitatibus sed"
+    },
+    {
+        title: "rerum commodi",
+        body: "occaecati laudantium ratione non cumque\nearum quod non enim soluta nisi velit similique voluptatibus\nesse laudantium consequatur voluptatem rem eaque voluptatem aut ut\net sit quam"
+    },
+    {
+        title: "consequatur animi",
+        body: "illum et alias quidem magni voluptatum\nab soluta ea qui saepe corrupti hic et\ncum repellat esse\nest sint vel veritatis officia consequuntur cum"
+    },
+    {
+        title: "rerum placeat",
+        body: "id est iure occaecati quam similique enim\nab repudiandae non\nillum expedita quam excepturi soluta qui placeat\nperspiciatis optio maiores non doloremque aut iusto sapiente"
+    },
+    {
+        title: "dolorum soluta",
+        body: "eum accusamus aut delectus\narchitecto blanditiis quia sunt\nrerum harum sit quos quia aspernatur vel corrupti inventore\nanimi dicta vel corporis"
+    }
+]
+
+function commentsStructure(arr) {
+
+    for (const comment of arr) {
+        let mainDiv = document.createElement("div");
+        let titleDiv = document.createElement("div");
+        let bodyDiv = document.createElement("div");
+
+        titleDiv.innerText = `Title: ${comment.title}`;
+        bodyDiv.innerText = `Body: ${comment.body}`;
+
+        mainDiv.append(titleDiv, bodyDiv);
+
+        let btn = document.createElement("button");
+        btn.innerText = 'Hide body';
+        mainDiv.appendChild(btn);
+        let counter = 0;
+        btn.onclick = function () {
+            counter++
+            if (counter % 2 !== 0) {
+                bodyDiv.style.display = 'none';
+            } else {
+                bodyDiv.style.display = 'block';
+            }
+        }
+        document.body.appendChild(mainDiv);
+    }
+}
+
+// commentsStructure(commentsArr);
+
 // - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
-//
-//
-//
-//
+
+function btnForDisappear() {
+    let btn = document.createElement("button");
+    btn.innerText = `I\'ll disappear`;
+    document.body.appendChild(btn);
+    btn.onclick = function () {
+        this.style.display = 'none';
+    }
+}
+
+// btnForDisappear();
+
 // - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
 //     При лівому кліку миші вивести в консоль інформацію про блок або елемент на який відбувся клік.
 //     Інформація яку потрібно вивести: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
-//
+
+document.onclick = function (ev) {
+    console.log(`Назва тегу: ${ev.target.localName}. Список класів: ${ev.target.className}. Список ід: ${ev.target.id}. Розмір: ${ev.target.offsetHeight * ev.target.offsetWidth}`);
+}
+
 // - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
 //     При лівому кліку миші  зробить popup (спливаючий блок) в якому буде вся інформація про блок.
 //     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
-//
+
+
+
 // -- взять массив пользователей
 // let usersWithAddress = [
 //     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
